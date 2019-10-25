000794344350e608f8c5ce68278af31a9d8ff0fe
Paul Paginton <pagyP@users.noreply.github.com>
2019-10-25 16:32:50 +0100

    updaed shell script

--- OpenVPN Server/install_openvpn_access-server.sh
+++ OpenVPN Server/install_openvpn_access-server.sh
@@ -7,19 +7,19 @@ userPassword=$1
 
 #install the software
 #sudo dpkg -i openvpn-as-2.1.9-Ubuntu16.amd_64.deb
-sudo apt update && apt -y install ca-certificates wget net-tools
-cd /tmp
+sudo su
+apt update && apt -y install ca-certificates wget net-tools
 wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
 echo "deb http://as-repository.openvpn.net/as/debian bionic main">/etc/apt/sources.list.d/openvpn-as-repo.list
-sudo apt update && apt -y install openvpn-as
+apt update && apt -y install openvpn-as
 
 #update the password for user openvpn
 sudo echo "openvpn:$userPassword"|sudo chpasswd
 
 #configure server network settings
-PUBLICIP=$(curl -s ifconfig.me)
-sudo apt-get install sqlite3
-sudo sqlite3 "/usr/local/openvpn_as/etc/db/config.db" "update config set value='$PUBLICIP' where name='host.name';"
+#PUBLICIP=$(curl -s ifconfig.me)
+#sudo apt-get install sqlite3
+#sudo sqlite3 "/usr/local/openvpn_as/etc/db/config.db" "update config set value='$PUBLICIP' where name='host.name';"
 
 #restart OpenVPN AS service
-sudo systemctl restart openvpnas
+#sudo systemctl restart openvpnas
